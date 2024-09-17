3cf0b8e526c9f4b3d725eb16a20397b7c854115e
zalogarciam <gegarciam95@gmail.com>
2024-09-16 20:41:20 -0500

    Loading less for the project

--- less-project/next.config.mjs
+++ less-project/next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+import withLess from "next-with-less";
+
+export default withLess({
+  lessLoaderOptions: {
+    lessOptions: {
+      javascriptEnabled: true,
+    },
+  },
+});
